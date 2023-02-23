430369771c64f4edaceba34162d733b1788fd802
Josiah Webb <jj.webb520@gmail.com>
2023-02-23 14:09:34 -0700

    rough implementation of scroll controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,45 +1,52 @@
-import { Float, OrbitControls, Sky, Text } from '@react-three/drei'
+import { Float, OrbitControls, Scroll, ScrollControls, Sky, Text } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import Ocean from './components/Ocean'
 import Section from './components/Section'
 
 export default function Experience() {
 
+    const { width, height } = useThree((state) => state.viewport)
+
     return (
         <>
-            <OrbitControls makeDefault />
+            {/* <OrbitControls makeDefault /> */}
             {/* <Perf position='top-left' /> */}
             
             <Ocean />
-            <Section />
-            <Float
-                position={[ -2, 2, 2 ]}
-            >
-                <Text color='black'>
-                    About
-                </Text>
-            </Float>
-            <Float
-                position={[ -.75, 3.25, -4 ]}
-            >
-                <Text color='black'>
-                    Experience
-                </Text>
-            </Float>
-            <Float
-                position={[ 2, .9, 1 ]}
-            >
-                <Text color='black'>
-                    Contact
-                </Text>
-            </Float>
-            <Float
-                position={[ -10, .5, -8 ]}
-            >
-                <Text color='black'>
-                    Projects
-                </Text>
-            </Float>
+            {/* <Section /> */}
+            <ScrollControls pages={4} infinite>
+                <Scroll>
+                    <Float
+                        position={[ -2, 2, 2 ]}
+                    >
+                        <Text color='black'>
+                            About
+                        </Text>
+                    </Float>
+                    <Float
+                        position={[ -.75, -height, -4 ]}
+                    >
+                        <Text color='black'>
+                            Experience
+                        </Text>
+                    </Float>
+                    <Float
+                        position={[ 2, -height * 2, 1 ]}
+                    >
+                        <Text color='black'>
+                            Contact
+                        </Text>
+                    </Float>
+                    <Float
+                        position={[ -10, -height * 3, -8 ]}
+                    >
+                        <Text color='black'>
+                            Projects
+                        </Text>
+                    </Float>
+                </Scroll>
+            </ScrollControls>
         </>
     )
 }
