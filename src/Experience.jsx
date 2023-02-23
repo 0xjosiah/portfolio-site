45f14d6de8c5da30d9a50cb73c3915b5bc17ea70
Josiah Webb <jj.webb520@gmail.com>
2023-02-22 20:06:56 -0700

    cleans up experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,36 +1,16 @@
 import { OrbitControls, Sky } from '@react-three/drei'
-import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import Ocean from './components/Ocean'
 import Section from './components/Section'
 
 export default function Experience() {
-    /* sky controls */
-    // const { turbidity, rayleigh, mieCoefficient, mieDirectionalG, elevation, azimuth, exposure } = useControls({
-    //     turbidity: 10,
-    //     rayleigh: 3,
-    //     mieCoefficient: 0.005,
-    //     mieDirectionalG: .7,
-    //     elevation: 2,
-    //     azimuth: 180,
-    //     exposure: .5,
-    // })
 
     return (
         <>
             <OrbitControls makeDefault />
-            <Perf position='top-left' />
+            {/* <Perf position='top-left' /> */}
             
             <Ocean />
-            {/* <Sky 
-                turbidity={ turbidity }
-                rayleigh={ rayleigh }
-                mieCoefficient={ mieCoefficient }
-                mieDirectionalG={ mieDirectionalG }
-                elevation={ elevation }
-                azimuth={ azimuth }
-                exposure={ exposure }
-            /> */}
             <Section />
         </>
     )
