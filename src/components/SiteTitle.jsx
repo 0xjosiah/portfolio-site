3f7d2f372a81182336a6a4f45af08d094d1137f2
Josiah Webb <jj.webb520@gmail.com>
2023-02-23 21:33:12 -0700

    cleans siteTitle

--- src/components/SiteTitle.jsx
+++ src/components/SiteTitle.jsx
@@ -1,25 +1,6 @@
-import { FontLoader } from 'three/addons/loaders/FontLoader.js'
-import * as THREE from 'three'
-import { useEffect, useState } from 'react'
 import { Text3D, useTexture } from '@react-three/drei'
 
-const fontLoader = new FontLoader()
-const font = fontLoader.load(
-    // font path
-    './fonts/dosis/Dosis_Regular.json',
-
-    // onLoad callback
-    (font) => font
-)
-
 export default function SiteTitle(props) {
-    // const [ font, setFont ] = useState()
-    // useEffect(() => {
-    //     new FontLoader().load(
-    //         './fonts/dosis/Dosis_Regular.json',
-    //         (font) => setFont(font)
-    //     )
-    // }, [])
     const text = props.text
     const textOptions = {
         size: .5,
