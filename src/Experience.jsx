a4cb475245f7c4fafe5ffa2893b425dede411831
Josiah Webb <jj.webb520@gmail.com>
2023-02-28 17:45:00 -0700

    attempt at mask

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,8 @@
-import { Billboard, Float, GradientTexture, Html, OrbitControls, Scroll, ScrollControls, Sky, Text, Text3D, Trail } from '@react-three/drei'
+import { Billboard, Float, GradientTexture, Html, Mask, OrbitControls, Scroll, ScrollControls, Sky, Text, Text3D, Trail, useMask } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
+import { PlaneGeometry } from 'three'
 import { Fish } from './components/Fish'
 import Ocean from './components/Ocean'
 import Section from './components/Section'
@@ -11,20 +12,37 @@ export default function Experience() {
 
     const { width, height } = useThree((state) => state.viewport)
 
-    // const { position, rotation } = useControls({
-    //     position: [ 0, 0, 0 ],
-    //     rotation: [ 0, 0, 0 ]
-    // })
+    const { position, rotation, invert } = useControls({
+        position: [ 0, 0, 0 ],
+        rotation: [ 0, 0, 0 ],
+        invert: false
+    })
+
+    const stencil = useMask(1, invert)
+
+    const {x, y, z} = useThree((state) => state.camera.position)
+    // console.log(camPos)
 
     return (
         <>
             {/* <OrbitControls makeDefault /> */}
             {/* <Perf position='top-left' /> */}
             
-            <Ocean />
-            <SiteTitle text='0xJosiah' position={ [ -0.35, -0.2, 4.5 ] } rotation={ [ 0, 1, 0.09 ] }/>
+            <Ocean {...stencil} />
+            <SiteTitle text='0xJosiah' position={ [ -0.35, -0.2, 4.5 ] } rotation={ [ 0, 1, 0.09 ] } {...stencil} />
             
             {/* <Section /> */}
+            <Mask id={1} invert={false}>
+                <mesh
+                    position={[ 3.5, 0.5, 6.5 ]}
+                >
+                    <planeGeometry />
+                    {/* <meshBasicMaterial */}
+                        {/* // color='red' */}
+                    {/* /> */}
+                </mesh>
+            </Mask>
+
 
             <ScrollControls pages={6} infinite horizontal>
                 <Scroll>
