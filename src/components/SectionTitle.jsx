a8790f757b8c0683a8ec5297f5a0db8f7aa5d2f8
Josiah Webb <jj.webb520@gmail.com>
2023-03-02 16:44:38 -0700

    implements useNavigate in sectionTitle

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,13 +1,19 @@
 import { Float } from "@react-three/drei"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import SiteTitle from "./SiteTitle"
 
 export default function SectionTitle({ text, linkTo, position }) {
 
+    const navigate = useNavigate()
+
+    const clickNav = () => {
+        navigate(linkTo)
+    }
+
     return (
         // <Link to={linkTo}>
-            <Float position={position}>
-                <SiteTitle text={text} />
+            <Float position={position} onClick={clickNav}>
+                <SiteTitle text={text} linkTo={linkTo}/>
             </Float>
         // </Link>
         /* maybe add link in html tag in SiteTitle */
