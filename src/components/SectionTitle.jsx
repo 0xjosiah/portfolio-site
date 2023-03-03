6305c2725d068cbfef6be88f2ebd69c9c2dbe54c
Josiah Webb <jj.webb520@gmail.com>
2023-03-02 17:17:02 -0700

    cleans up sectionTitle

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,4 @@
 import { Float } from "@react-three/drei"
-import { Link, useNavigate } from "react-router-dom"
 import SiteTitle from "./SiteTitle"
 
 export default function SectionTitle({ text, navTo, position }) {
