68a9469408ccf0216c119fbb0b04a714096785b7
Josiah Webb <jj.webb520@gmail.com>
2023-03-02 17:17:50 -0700

    sets default props for SectionTitle

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 import { Float } from "@react-three/drei"
 import SiteTitle from "./SiteTitle"
 
-export default function SectionTitle({ text, navTo, position }) {
+export default function SectionTitle({ text='Title', navTo='/', position=[ 0, 0, 0 ] }) {
 
     return (
         <Float position={position}>
