57c2e89c10caa9b14f24fe5ccdbe66fdfd67a16f
Josiah Webb <jj.webb520@gmail.com>
2023-03-08 17:24:34 -0700

    adds state and makes form controlled

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,25 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function Contact() {
     const form = useRef(null)
+    const [ formData, setFormData ] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
 
     const sendEmail = (e) => {
         e.preventDefault()
         /* TODO */
+    }
+
+    const updateFormData = (e) => {
+        const { name, value } = e.target 
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+        console.log(formData)
     }
 
     const styles = {
@@ -17,11 +31,26 @@ export default function Contact() {
     return (
         <form ref={form} onSubmit={sendEmail} style={{...styles}}>
             <label>Name</label>
-            <input autoFocus required type='text' />
+            <input
+                autoFocus
+                required
+                type='text'
+                name='name'
+                onChange={updateFormData}
+            />
             <label>Email</label>
-            <input required type='email' />
+            <input
+                required
+                type='email'
+                name='email'
+                onChange={updateFormData}
+            />
             <label>Message</label>
-            <textarea required  />
+            <textarea
+                required
+                name='message'
+                onChange={updateFormData} 
+            />
             <button>Send</button>
         </form>
     )
