b08df7d55036f0e78fd9521bfb06c998fc4c4a32
Josiah Webb <jj.webb520@gmail.com>
2023-03-09 15:57:34 -0700

    creates a stage for contact stuff in scene

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,10 +1,11 @@
-import { Scroll, ScrollControls } from '@react-three/drei'
+import { Html, PivotControls, RoundedBox, Scroll, ScrollControls } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import Ocean from './components/Ocean'
 import SectionTitle from './components/SectionTitle'
 import SiteTitle from './components/SiteTitle'
+import Contact from './pages/Contact'
 
 export default function Scene() {
 
@@ -18,7 +19,10 @@ export default function Scene() {
 
     /* Camera position if needed */
     // const { position } = useThree((state) => state.camera)
+    // const { camera } = useThree((state) => state)
     // console.log('camPos', position)
+
+    const contactStagePos = [ 3.50, -0.25, 6 ]
 
     return (
         <>
@@ -27,6 +31,19 @@ export default function Scene() {
             
             <Ocean />
             <SiteTitle text='0xJosiah' navTo='/' position={[ -0.35, -0.2, 4.5 ]} rotation={[ 0, 1, 0.09 ]} />
+            {/* <PivotControls
+                anchor={[ 0, 1, 0 ]}
+            > */}
+                <RoundedBox
+                    args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+                    radius={0.05} // Radius of the rounded corners. Default is 0.05
+                    smoothness={4} // The number of curve segments. Default is 4
+                    position={contactStagePos}
+                >
+                    <meshBasicMaterial color="#f3f3f3" />
+                </RoundedBox>
+
+            {/* </PivotControls> */}
             
             {/* <Section position={[ 3.5, 0.5, 6.5 ]} /> */}
 
