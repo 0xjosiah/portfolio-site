38ac93858b35b4b9f278645c7923faba57d2b584
Josiah Webb <jj.webb520@gmail.com>
2023-03-09 22:43:18 -0700

    adds position for contact title

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -51,7 +51,7 @@ export default function Scene() {
 
             <Ocean />
             <SiteTitle text='0xJosiah' navTo='/' position={[ -0.35, -0.2, 4.5 ]} rotation={[ 0, 1, 0.09 ]} />
-            <SiteTitle text='CONTACT' navTo='/contact' position={position} rotation={rotation} size={.05} height={.03} bevelThickness={0.007} bevelSize={.004} letterSpacing={.006} />
+            <SiteTitle text='CONTACT' navTo='/contact' position={[ 3.6, 0.15, 6.5 ]} rotation={rotation} size={.05} height={.03} bevelThickness={0.007} bevelSize={.004} letterSpacing={.006} />
             {/* <PivotControls
                 anchor={[ 0, 1, 0 ]}
             >
