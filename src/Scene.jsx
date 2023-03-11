0912844ca1cc775e64d715a230c5b36f2894d05e
Josiah Webb <jj.webb520@gmail.com>
2023-03-10 22:26:50 -0700

    creates an anchor for contact title

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,4 +1,4 @@
-import { Box, Cylinder, FirstPersonControls, Html, PivotControls, RoundedBox, Scroll, ScrollControls, Svg, useTexture } from '@react-three/drei'
+import { Box, Cylinder, FirstPersonControls, Html, OrbitControls, PivotControls, RoundedBox, Scroll, ScrollControls, Svg, useTexture } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
@@ -52,7 +52,12 @@ export default function Scene() {
             <Ocean />
             <SiteTitle text='0xJosiah' navTo='/' position={[ -0.35, -0.2, 4.5 ]} rotation={[ 0, 1, 0.09 ]} />
             <SiteTitle text='CONTACT' navTo='/contact' position={[ 3.6, 0.15, 6.5 ]} size={.05} height={.03} bevelThickness={0.005} bevelSize={.003} letterSpacing={.005} />
-            {/* <Cylinder position={[ 3.6, 0.15, 6.5 ]} /> */}
+            <RoundedBox position={[ 3.746, 0.029, 6.515 ]} args={[ .0095, 0.25, 0.035 ]}
+                radius={0.0025} // Radius of the rounded corners. Default is 0.05
+                smoothness={4}
+            >
+                <meshMatcapMaterial matcap={chrome} />
+            </RoundedBox>
             {/* <PivotControls
                 anchor={[ 0, 1, 0 ]}
             >
