01f33fe7d5bed41a2d4457a3475347018c2a20dd
Josiah Webb <jj.webb520@gmail.com>
2023-03-14 16:43:23 -0600

    implements page scaffold on all other pages

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,17 +1,15 @@
+import PageScaffold from "../components/PageScaffold";
+
 export default function Experience() {
-    const styles = {
-        position: 'absolute',
-        top: 5,
-        left: 5
-    }
+
     return (
-        <div style={{...styles}}>
+        <PageScaffold>
             <h2 className="page-title">
                 Experience
             </h2>
             <p className="page-content">
                 This page is alll about my experience!
             </p>
-        </div>
+        </PageScaffold>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,10 @@
+import PageScaffold from "../components/PageScaffold";
+
 export default function Projects() {
-    const styles = {
-        position: 'absolute',
-        top: 5,
-        left: 5
-    }
+    
     return (
-        <div style={{...styles}}>
+        <PageScaffold>
             Projects
-        </div>
+        </PageScaffold>
     )
 }
