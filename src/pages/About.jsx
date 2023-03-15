8288b915acb0a1d1d851c8762eeebb51404e610e
Josiah Webb <jj.webb520@gmail.com>
2023-03-14 21:01:23 -0600

    refactors callout styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,16 +11,13 @@ export default function About() {
                 You'd be an absolute nob not to hire me. <br/>
                 This page is alll about me! <br/>
                 I am the shit, plain and simple. <br/>
-                You'd be an absolute nob not to hire me. <br/>
+                You'd be an <span className="keyword-callout">absolute</span> nob not to hire me. <br/>
                 This page is alll about me! <br/>
                 I am the shit, plain and simple. <br/>
-                You'd be an absolute nob not to hire me. <br/>
-                This page is alll about me! <br/>
-                I am the shit, plain and simple. <br/>
-                You'd be an absolute nob not to hire me. <br/>
-                This page is alll about me! <br/>
-                I am the shit, plain and simple. <br/>
-                You'd be an absolute nob not to hire me. <br/>
+                <span className="keyword-callout">You'd</span> be an absolute nob not to hire me. <br/>
+                This page is alll about me!
+                I am the shit, plain and simple.
+                You'd be an absolute nob not to <span className="keyword-callout">hire me</span> .
             </p>
             {/* <p className="page-content">
                 This page is alll about me! <br/>
