084c8d26641f6974883672662ddd4083dd6a7f6e
Josiah Webb <jj.webb520@gmail.com>
2023-03-15 17:07:24 -0600

    fixes nesting issues in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,42 +4,44 @@ export default function Projects() {
     
     return (
         <PageScaffold title="Projects">
-            <p className="page-content">
+            <div className="page-content">
                 <span className="keyword-callout">Project 1</span><br/>
-                This page is alll about my experience!
+                <p>
+                    A nice proj description.
+                </p>
                 <ul>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                 </ul>
-            </p>
-            <p className="page-content">
                 <span className="keyword-callout">Proj 2</span><br/>
-                This page is alll about my experience!
+                <p>
+                    A nice proj description.
+                </p>
                 <ul>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                 </ul>
-            </p>
-            <p className="page-content">
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
+                <span className="keyword-callout">Proj 3</span><br/>
+                <p>
+                    A nice proj description.
+                </p>
                 <ul>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                 </ul>
-            </p>
-            <p className="page-content">
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
+                <span className="keyword-callout">Proj 4</span><br/>
+                <p>
+                    A nice proj description.
+                </p>
                 <ul>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                     <li>this, that, and the other</li>
                 </ul>
-            </p>
+            </div>
         </PageScaffold>
     )
 }
