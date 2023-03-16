c74fdeb7e2ca97f737118ded5ca2d2cef2ff7ca3
Josiah Webb <jj.webb520@gmail.com>
2023-03-16 16:24:27 -0600

    playing with exp styling

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -9,24 +9,39 @@ export default function Experience() {
                     A little about my experience and how it can help any team...
                 </p>
             </div>
-            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', fontWeight: 'normal', borderBottom: '0px solid white', padding: '1em'}}>
+            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', fontWeight: 'normal', borderBottom: '0px solid white', padding: '1em', alignItems: 'center', color: '#fff'}}>
                 <div>
-                    <span className="job-callout">TIAA</span><br/>
+                    <span className="job-callout">Job 1</span><br/>
                 </div>
                 <div>
                     {/* <span className="keyword-callout"> */}
-                        TIAA
+                        Job 2
                     {/* </span> */}
                 </div>
                 <div>
                     {/* <span className="keyword-callout"> */}
-                        TIAA
+                        Job 3
+                    {/* </span> */}
+                </div>
+                <div>
+                    {/* <span className="keyword-callout"> */}
+                        Job 4
+                    {/* </span> */}
+                </div>
+                <div>
+                    {/* <span className="keyword-callout"> */}
+                        Job 5
+                    {/* </span> */}
+                </div>
+                <div>
+                    {/* <span className="keyword-callout"> */}
+                        Job 6
                     {/* </span> */}
                 </div>
             </div>
             <div className="page-content">
                 <div style={{ padding: '1em'}}>
-                    {/* <span className="keyword-callout">TIAA</span><br/> */}
+                    {/* <span className="keyword-callout">Job 1</span><br/> */}
                     Very Important Job Title
                     <ul>
                         <li>this, that, and the other</li>
