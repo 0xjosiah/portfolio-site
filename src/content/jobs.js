29ffa00871d0ccdb3e05626b82c766b8047d03e4
Josiah Webb <jj.webb520@gmail.com>
2023-03-16 17:28:11 -0600

    creates jobs content, maps company names into buttons

---
+++ src/content/jobs.js
@@ -0,0 +1,72 @@
+export const jobs = [
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+    {
+        company: 'Company',
+        title: 'Job Title',
+        dates: '2008 - 2009',
+        duties: [
+            'this, that, and the other',
+            'this, that, and the other',
+            'this, that, and the other',
+        ]
+    },
+]
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,6 +1,17 @@
-import PageScaffold from "../components/PageScaffold";
+import PageScaffold from "../components/PageScaffold"
+import { jobs } from "../content/jobs"
 
 export default function Experience() {
+    const companiesList = jobs.map(i => i.company)
+    const buttons = companiesList.map(i => (
+        <button className="job-btn">
+            <span className="job-callout">
+                {i}
+            </span>
+        </button>
+    ))
+
+    console.log(buttons)
 
     return (
         <PageScaffold title="Experience">
@@ -11,7 +22,9 @@ export default function Experience() {
             </div>
             <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', fontWeight: 'normal', borderBottom: '0px solid white', padding: '1em', alignItems: 'center', color: '#fff'}}>
                 <div>
-                    <span className="job-callout">Job 1</span><br/>
+                    <span className="job-callout">
+                        Job 1
+                    </span><br/>
                 </div>
                 <div>
                     {/* <span className="keyword-callout"> */}
@@ -40,9 +53,12 @@ export default function Experience() {
                 </div>
             </div>
             <div className="page-content">
-                <div style={{ padding: '1em'}}>
+                <div>
                     {/* <span className="keyword-callout">Job 1</span><br/> */}
-                    Very Important Job Title
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <p>Job Title @ <a>Company</a></p>
+                        <p>Date - 2009</p>
+                    </div>
                     <ul>
                         <li>this, that, and the other</li>
                         <li>this, that, and the other</li>
