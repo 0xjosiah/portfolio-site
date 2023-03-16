5c9966e7dafd36e4532c467d290a74f420adbd46
Josiah Webb <jj.webb520@gmail.com>
2023-03-16 15:41:43 -0600

    changes callout styling, adds padding to page title, creates dummy layout for experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,34 +5,33 @@ export default function Experience() {
     return (
         <PageScaffold title="Experience">
             <div className="page-content">
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
-                <ul>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                </ul>
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
-                <ul>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                </ul>
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
-                <ul>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                </ul>
-                <span className="keyword-callout">TIAA</span><br/>
-                This page is alll about my experience!
-                <ul>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                    <li>this, that, and the other</li>
-                </ul>
+                <p>
+                    A little about my experience and how it can help any team...
+                </p>
+                <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', fontWeight: 'bold', borderBottom: '0px solid white', padding: '1em'}}>
+                    <div>
+                        <span className="keyword-callout">TIAA</span><br/>
+                    </div>
+                    <div style={{fontWeight: 'bold'}}>
+                        {/* <span className="keyword-callout"> */}
+                            TIAA
+                        {/* </span> */}
+                    </div>
+                    <div>
+                        {/* <span className="keyword-callout"> */}
+                            TIAA
+                        {/* </span> */}
+                    </div>
+                </div>
+                <div style={{ padding: '1em'}}>
+                    {/* <span className="keyword-callout">TIAA</span><br/> */}
+                    Very Important Job Title
+                    <ul>
+                        <li>this, that, and the other</li>
+                        <li>this, that, and the other</li>
+                        <li>this, that, and the other</li>
+                    </ul>
+                </div>
             </div>
         </PageScaffold>
     )
