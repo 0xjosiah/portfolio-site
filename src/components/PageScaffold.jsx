39a76142ac504d832b8f24dbc352a79f386d57e1
Josiah Webb <jj.webb520@gmail.com>
2023-03-17 21:49:13 -0600

    cleans up page scaffold, adds default props for page scaffold

--- src/components/PageScaffold.jsx
+++ src/components/PageScaffold.jsx
@@ -2,38 +2,22 @@ import { BsArrowDownLeftSquareFill } from "react-icons/bs"
 import { Link } from "react-router-dom"
 import Socials from "./Socials"
 
-export default function PageScaffold({ children, title, socials, noTitle }) {
+export default function PageScaffold({ children, title = 'page title', socials = false, noTitle = false }) {
     return (
         <div
             className={noTitle ? "page-scaffold no-title" : "page-scaffold"}
         >
-            {/* <div
-                style={{
-                    // overflow: 'auto',
-                    // position: 'absolute',
-                    // top: 0,
-                    // left: 0,
-                    // width: '100%'
-                    // maxHeight: '2.5rem'
-                    // flex: '1',
-                }}
-            > */}
-                <Link to="/" className="home-btn">
-                    <BsArrowDownLeftSquareFill />
-                </Link>
-                {/* {title && */}
-                    <h2 className="page-title">
-                        {/* the callout looks kinda nice */}
-                        {/* <span className="keyword-callout"> */}
-                            {title}
-                        {/* </span> */}
-                    </h2>
-                {/* } */}
-            {/* </div> */}
+            <Link to="/" className="home-btn">
+                <BsArrowDownLeftSquareFill />
+            </Link>
+            <h2 className="page-title">
+                {/* the callout looks kinda nice */}
+                {/* <span className="keyword-callout"> */}
+                    {title}
+                {/* </span> */}
+            </h2>
 
-            <>
-                {children}
-            </>
+            {children}
 
             {socials &&
                 <Socials />
