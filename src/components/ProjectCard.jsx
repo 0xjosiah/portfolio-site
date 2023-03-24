26759d47a830caede2d1ba86bc39913ce6377a24
Josiah Webb <jj.webb520@gmail.com>
2023-03-23 22:21:27 -0600

    dynamically renders tags

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,11 +2,12 @@ import { HiOutlineCode, HiOutlineExternalLink } from "react-icons/hi"
 import { useHover } from "../hooks/useHover"
 import ProjectTag from "./ProjectTag"
 
-export default function ProjectCard({ img }) {
+export default function ProjectCard({ img, tags }) {
     const [hoverRef, isHovered] = useHover()
 
     /* TODO: delete this */
     img = 'url("../images/Screenshot_test2.png")'
+    tags = ['react', 'react 3 fiber', 'css', 'cannon js', 'supercool']
 
     return (
         <>
@@ -40,39 +41,7 @@ export default function ProjectCard({ img }) {
             {/* Card footer - Keywords and Links */}
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'start' }}>
                 <div style={{ display: 'inherit', maxWidth: '65%', flexWrap: 'wrap' }}>
-                    <ProjectTag tag={"react"} />
-                    <p
-                        style={{
-                            margin: '1rem .5rem 0 0',
-                            color: 'white',
-                        }}
-                    >
-                        <span className="keyword-callout">React 3 Fiber</span><br/>
-                    </p>
-                    <p
-                        style={{
-                            margin: '1rem .5rem 0 0',
-                            color: 'white',
-                        }}
-                    >
-                        <span className="keyword-callout">Cannon js</span><br/>
-                    </p>
-                    <p
-                        style={{
-                            margin: '1rem .5rem 0 0',
-                            color: 'white',
-                        }}
-                    >
-                        <span className="keyword-callout">CSS</span><br/>
-                    </p>
-                    <p
-                        style={{
-                            margin: '1rem .5rem 0 0',
-                            color: 'white',
-                        }}
-                    >
-                        <span className="keyword-callout">SuperCool</span><br/>
-                    </p>
+                    {tags.map(tag => <ProjectTag tag={tag} />)}
                 </div>
                 <div style={{ display: "flex" }}>
                     <a href="https://github.com" alt="link to project code" target="_blank">
