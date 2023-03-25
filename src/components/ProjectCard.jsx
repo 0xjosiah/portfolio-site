4a18e3d8540cf17d9d062c0d50193f4128ab7f8d
Josiah Webb <jj.webb520@gmail.com>
2023-03-24 20:23:59 -0600

    refines styles for proj card, adds props for desc, prob, and solution

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,12 +3,18 @@ import { useHover } from "../hooks/useHover"
 import ProjectLinks from "./ProjectLinks"
 import ProjectTag from "./ProjectTag"
 
-export default function ProjectCard({ links, img, tags }) {
+export default function ProjectCard({ description, links, img, problem, solution, tags }) {
     const [hoverRef, isHovered] = useHover()
 
     /* TODO: delete this */
     img = 'url("../images/Screenshot_test2.png")'
     tags = ['react', 'react 3 fiber', 'css', 'cannon js', 'supercool']
+
+    description = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores rem veniam molestiae! Nesciunt cum beatae, saepe molestias labore voluptatum vero repellat accusantium magnam, voluptatem sunt, officia ipsum ea ullam.`
+
+    problem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. In saepe ipsam ea, doloribus atque, facilis vitae officia voluptate assumenda commodi natus beatae unde ex, velit fuga quasi quo. Debitis, voluptate.`
+
+    solution = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis ullam ipsam impedit possimus officia a quisquam pariatur quaerat, voluptatem quasi error inventore perferendis necessitatibus delectus veritatis rerum excepturi numquam!`
 
     return (
         <>
@@ -20,7 +26,9 @@ export default function ProjectCard({ links, img, tags }) {
                     backgroundImage: img,
                 }}
             >
-                <h3 className="project-card-title">Super Dope Project</h3>
+                <h3 className={`${isHovered ? 'project-card-title no-br' : 'project-card-title'}`}>
+                    Super Dope Project
+                </h3>
                 <div
                     style={{
                         display: `${isHovered ? 'block' : 'none'}`
@@ -28,14 +36,13 @@ export default function ProjectCard({ links, img, tags }) {
                     className="project-card-description"
                 >
                     <p>
-                        Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores rem veniam molestiae! Nesciunt cum beatae, saepe molestias labore voluptatum vero repellat accusantium magnam, voluptatem sunt, officia ipsum ea ullam.
+                        Description: {description}
                     </p>
                     <p>
-                        Problem: Lorem ipsum dolor sit amet consectetur adipisicing elit. In saepe ipsam ea, doloribus atque, facilis vitae officia voluptate assumenda commodi natus beatae unde ex, velit fuga quasi quo. Debitis, voluptate.
+                        Problem: {problem}
                     </p>
                     <p>
-                        Solution:
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis ullam ipsam impedit possimus officia a quisquam pariatur quaerat, voluptatem quasi error inventore perferendis necessitatibus delectus veritatis rerum excepturi numquam!
+                        Solution: {solution}
                     </p>
                 </div>
             </div>
