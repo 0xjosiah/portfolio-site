2716e1c3ce4058bc2a21f0d76c18349c2932a5a8
Josiah Webb <jj.webb520@gmail.com>
2023-03-24 20:31:58 -0600

    refactors img prop for proj card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,3 @@
-import { HiOutlineCode, HiOutlineExternalLink } from "react-icons/hi"
 import { useHover } from "../hooks/useHover"
 import ProjectLinks from "./ProjectLinks"
 import ProjectTag from "./ProjectTag"
@@ -7,7 +6,7 @@ export default function ProjectCard({ description, links, img, problem, solution
     const [hoverRef, isHovered] = useHover()
 
     /* TODO: delete this */
-    img = 'url("../images/Screenshot_test2.png")'
+    img = "../images/Screenshot_test2.png"
     tags = ['react', 'react 3 fiber', 'css', 'cannon js', 'supercool']
 
     description = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores rem veniam molestiae! Nesciunt cum beatae, saepe molestias labore voluptatum vero repellat accusantium magnam, voluptatem sunt, officia ipsum ea ullam.`
@@ -23,7 +22,7 @@ export default function ProjectCard({ description, links, img, problem, solution
                 ref={hoverRef}
                 className="project-card-container"
                 style={{
-                    backgroundImage: img,
+                    backgroundImage: `url(${img})`,
                 }}
             >
                 <h3 className={`${isHovered ? 'project-card-title no-br' : 'project-card-title'}`}>
@@ -49,8 +48,8 @@ export default function ProjectCard({ description, links, img, problem, solution
 
             {/* Card footer - Keywords and Links */}
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'start' }}>
-                <div style={{ display: 'inherit', maxWidth: '65%', flexWrap: 'wrap' }}>
-                    {tags.map((tag, index) => <ProjectTag tag={tag} key={index} />)}
+                <div style={{ display: "inherit", maxWidth: "65%", flexWrap: "wrap" }}>
+                    { tags.map((tag, index) => <ProjectTag tag={tag} key={index} />) }
                 </div>
                 <ProjectLinks links={links} />
             </div>
