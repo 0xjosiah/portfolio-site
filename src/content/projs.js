d69cce96ef7bf97ed572e2ee4672808a05a60130
Josiah Webb <jj.webb520@gmail.com>
2023-03-28 20:53:51 -0600

    adds proj photo, fills out more proj content

--- src/content/projs.js
+++ src/content/projs.js
@@ -2,7 +2,7 @@ const projects = [
     {
         title: 'Marble Combine',
         img: '../images/marble-combine.png',
-        tags: ['react', 'react 3 fiber', 'css', 'cannon js', 'supercool'],
+        tags: ['react', 'react 3 fiber', 'CSS', 'Rapier'],
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores rem veniam molestiae! Nesciunt cum beatae, saepe molestias labore voluptatum vero repellat accusantium magnam, voluptatem sunt, officia ipsum ea ullam.',
         problem: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In saepe ipsam ea, doloribus atque, facilis vitae officia voluptate assumenda commodi natus beatae unde ex, velit fuga quasi quo. Debitis, voluptate.',
         solution: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis ullam ipsam impedit possimus officia a quisquam pariatur quaerat, voluptatem quasi error inventore perferendis necessitatibus delectus veritatis rerum excepturi numquam!',
@@ -47,6 +47,18 @@ const projects = [
             demoLink: 'https://0xjosiah.xyz'
         },
     },
+    {
+        title: 'Dirty Ltd. Store',
+        img: '../images/dirty-store.png',
+        tags: ['JavaScript', 'CSS', 'Liquid'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores rem veniam molestiae! Nesciunt cum beatae, saepe molestias labore voluptatum vero repellat accusantium magnam, voluptatem sunt, officia ipsum ea ullam.',
+        problem: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In saepe ipsam ea, doloribus atque, facilis vitae officia voluptate assumenda commodi natus beatae unde ex, velit fuga quasi quo. Debitis, voluptate.',
+        solution: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis ullam ipsam impedit possimus officia a quisquam pariatur quaerat, voluptatem quasi error inventore perferendis necessitatibus delectus veritatis rerum excepturi numquam!',
+        links: {
+            codeLink: null,
+            demoLink: 'https://dirtyltd.co/'
+        },
+    }
 ]
 
 export { projects }
